2e5f4a2f221e8b93c50012e57d47881dc708c5c3
yuna <yuna744@gmail.com>
2024-09-15 15:44:47 +0900

    📝 add : npm setup

---
+++ public/npm.js
@@ -0,0 +1,17 @@
+// yarn add -D tailwindcss postcss autoprefixer
+// npx tailwindcss init -p   (이 명령어로 tailwind.config.js 파일을 생성합니다.)
+
+// yarn add json-server
+// yarn json-server db.json --port 5000
+
+// yarn add axios
+// yarn add @tanstack/react-query @tanstack/react-query-devtools
+// yarn add @tanstack/react-query@latest @tanstack/react-query-devtools@latest
+
+// yarn add react-router-dom
+
+// yarn add zustand
+// yarn add immer
+// yarn add lodash
+
+// yarn add react-icons
