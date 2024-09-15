5d03da89012cecfb574d5b3bc7b5c5bf3457612e
yuna <yuna744@gmail.com>
2024-09-15 15:57:26 +0900

    📝 add : 인증/인가 api 구성

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,33 @@
-export default function auth() {
-  return <div className="auth">auth</div>
+import axios from 'axios'
+
+const API_URL = 'https://moneyfulpublicpolicy.co.kr'
+
+export const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData)
+  return response.data
+}
+
+export const login = async (userData) => {
+  const response = await axios.post(`${API_URL}/login`, userData)
+  return response.data
+}
+
+export const getUserProfile = async (token) => {
+  const response = await axios.get(`${API_URL}/user`, {
+    headers: {
+      'Authorization:': `Bearer ${token}`
+    }
+  })
+  return response.data
+}
+
+export const updateProfile = async (formData) => {
+  const token = localStorage.getItem('accessToken')
+  const response = await axios.patch(`${API_URL}/profile`, formData, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization:': `Bearer ${token}`
+    }
+  })
+  return response.data
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,31 @@
-export default function Layout() {
-  return <div className="Layout">Layout</div>
+// Props 로 꼭 children 만 받을 필요는 없답니다.
+const Layout = ({ children }) => {
+  const navigate = useNavigate()
+
+  // 이곳에서 로그인 하지 않은 사용자를 login 페이지로 보내줄 거에요.
+  useEffect(() => {}, [])
+
+  const handleLogout = () => {}
+
+  return (
+    <div>
+      <header>
+        <nav>
+          <Link to="/">홈</Link>
+          <div className="space-x-4">
+            {user ? (
+              <>
+                <button onClick={handleLogout}>로그아웃</button>
+              </>
+            ) : (
+              <Link to="/login">로그인</Link>
+            )}
+          </div>
+        </nav>
+      </header>
+      <main className="container mx-auto pt-10 main">{children}</main>
+    </div>
+  )
 }
+
+export default Layout
