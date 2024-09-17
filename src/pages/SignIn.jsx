692cc66fd6f11609c134b0afe74d0f7ebfb1dc80
yuna <yuna744@gmail.com>
2024-09-18 04:58:36 +0900

    📝 add : 로그인, 회원가입 라우터 경로 설정

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,3 @@
+export default function SignIn() {
+  return <div className="SignIn">로그인 페이지 입니다</div>
+}
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,3 @@
+export default function SignUp() {
+  return <div className="SignUp">회원가입 페이지 입니다.</div>
+}
